9f4aa41736e623cc9afcba3c1a697c1ed8ae71c9
Chenul Dulmika Thenuwara <chenuldulmikathenuwara@gmail.com>
2024-01-24 14:40:41 +0530

    Q2 done

---
+++ Labsheet 01 Q2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Labsheet_01_Q2
+{
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Enter 10 integers:");
+
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.Write("Enter an integer: ");
+                    int number;
+
+                    if (int.TryParse(Console.ReadLine(), out number))
+                    {
+                        if (IsEven(number))
+                        {
+                            Console.WriteLine(number + " is even.");
+                        }
+                        else
+                        {
+                            Console.WriteLine(number + " is odd.");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input. Please enter a valid integer.");
+                        i--;
+                }
+
+                Console.ReadLine();
+            }
+
+            static bool IsEven(int number)
+            {
+                return number % 2 == 0;
+            }
+        }
+    }
+
+}
+    }1
+}
