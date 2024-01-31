30dbdd4869ff7ef7127daa8f617f3265c0534e01
Chenul Dulmika Thenuwara <chenuldulmikathenuwara@gmail.com>
2024-01-31 13:28:37 +0530

    Question 04 done

---
+++ Labsheet 01 Q3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labsheet_01_Q3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a positive number: ");
+            Number = int.Parse(Console.ReadLine());
+
+            if(Number>0)
+            {
+                for(int );
+            }
+            else
+            {
+                Console.WriteLine("The number is negative");
+            }
+        }
+    }
+}
---
+++ Labsheet 01 Q4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labsheet_01_Q4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.WriteLine("Enter a number to display its multiplication table:");
+
+                
+                if (int.TryParse(Console.ReadLine(), out int number))
+                {
+                    Console.WriteLine($"Multiplication table for {number}:");
+
+                    
+                    for (int i = 1; i <= 10; i++)
+                    {
+                        int result = number * i;
+                        Console.WriteLine($"{number} * {i} = {result}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid number.");
+                }
+
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
